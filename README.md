3720344729462d7ef687bed8e578bb21d30cd53d
vivien <27066260+vivienlouradour@users.noreply.github.com>
2019-11-05 23:27:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ADP bot messenger
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const login = require("facebook-chat-api")
+const fs = require("fs")
+const loginDataFile = "./login-data.json"
+
+const config = JSON.parse(fs.readFileSync("./config.json"))
+
+const loginDataExists = fs.existsSync(loginDataFile)
+let loginData
+if (loginDataExists) {
+    console.log('reading loginData from file')
+    loginData = { appState: JSON.parse(fs.readFileSync(loginDataFile)) }
+}
+else {
+    loginData = { email: config.facebook.email, password: config.facebook.password }
+}
+
+login(loginData, (err, api) => {
+    if (err) return console.error(err)
+
+    let appState = api.getAppState()
+    fs.writeFile(loginDataFile, JSON.stringify(appState, null, 2), err => {
+        if (err)
+            console.error('failed to save appState : ' + JSON.stringify(err))
+    })
+
+    api.listen((err, message) => {
+        console.log('Message received:' + message.body)
+        //api.sendMessage(message.body, message.threadID);
+    });
+});
