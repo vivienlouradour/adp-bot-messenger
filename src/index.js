7c31d78254ddda2b50e81964726c2f76a7f2059f
vivien <27066260+vivienlouradour@users.noreply.github.com>
2019-11-06 19:55:25 +0100

    add logger

--- src/index.js
+++ src/index.js
@@ -4,19 +4,29 @@ const loginDataFile = "./config/login-data.json"
 const command = require("./command/botCommand")
 const botMessage = require("./command/botMessage")
 const messageDelay = 60 * 60 * 6 * 1000
+// create a rolling file logger based on date/time that fires process events
+const opts = {
+    errorEventName: 'error',
+    logDirectory: './logs', // NOTE: folder must exist and be writable...
+    fileNamePattern: 'adp-bot-messenger-<DATE>.log',
+    dateFormat: 'YYYY.MM.DD'
+};
+const log = require('simple-node-logger').createRollingFileLogger(opts);
+
+log.info('Starting bot...')
 
 const loginDataExists = fs.existsSync(loginDataFile)
 let loginData
 if (loginDataExists) {
-    console.log('reading loginData from file')
+    log.info('reading loginData from file')
     loginData = { appState: JSON.parse(fs.readFileSync(loginDataFile)) }
 }
 else {
+    log.info('reading credentials from config.json file')
     const config = JSON.parse(fs.readFileSync("./config/config.json"))
     loginData = { email: config.facebook.email, password: config.facebook.password }
 }
 
-console.log(botMessage.getRandomMessage())
 login(loginData, (err, api) => {
     if (err) return console.error(err)
 
@@ -32,25 +42,26 @@ login(loginData, (err, api) => {
     setInterval(function () {
         api.getThreadList(999, null, [], (err, list) => {
             if (err) return console.error(err)
-            let threadIds = list.filter(thread => thread.isGroup).map(thread => thread.threadID)
+            let groupIds = list.filter(thread => thread.isGroup).map(group => group.threadID)
 
             let message = botMessage.getRandomMessage()
-            console.log(message)
-            console.log(JSON.stringify(threadIds))
-            threadIds.forEach(threadId => api.sendMessage(message, threadId))
+            log.info('sending automatic message to ' + groupIds.length + ' conversation(s)')
+            groupIds.forEach(groupId => api.sendMessage(message, groupId))
         })
     }, messageDelay);
 
     api.listen((err, message) => {
-        console.log('Message received (from id ' + message.senderID + '): ' + message.body)
         if (message.senderID == botId) {
-            console.log('message from me')
+
         }
         else {
             //parse message to see if it's command (!adp command)   
             if (command.isCommand(message.body)) {
+                log.info('Command received from sender ' + message.senderID + ' : ' + message.body)
+
                 let response = command.parseCommand(message.body)
-                api.sendMessage(response, message.threadID);
+                api.sendMessage(response, message.threadID)
+                log.info('Message sent to ' + message.senderID + ' : ' + response)
             }
         }
     })
